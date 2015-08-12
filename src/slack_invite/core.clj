86af5f250fb39fd2a4b2d81db3a5842a6157565a
SevereOverfl0w <overfl0w@narna.co>
2015-08-12 15:09:48 +0100

    add basic templates

--- src/slack_invite/core.clj
+++ src/slack_invite/core.clj
@@ -2,23 +2,28 @@
   (:require [bidi.bidi :as b]
             [bidi.ring :refer (make-handler)]
             [environ.core :refer [env]]
-            [clj-slack.core :as slack]))
+            [clj-slack.core :as slack])
+  (:use [selmer.parser]))
 
 (def slack-connection {:api-url (env :api-url) :token (env :api-token)})
+(def team-name (get env :team-name "Anonymous team"))
 
 (defn showForm
   [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "<h1>Hello world!</h1>"})
+   :body (render-file "templates/index.html" {})
+   })
 
 (defn handleForm
   [request]
-)
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Gratz"})
 
 (def routes
-  ["/" {:get showForm
-        :post handleForm}])
+  ["/" {:get #'showForm
+        :post #'handleForm}])
 
 (def handler
   (make-handler routes))
