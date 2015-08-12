81527801ff72fe27caee63f0249e2ba9f1a8f5bb
SevereOverfl0w <overfl0w@narna.co>
2015-08-12 13:39:21 +0100

    choose your weapons...

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.jelienxx/clj-slack "0.4.3"]]
+                 [org.julienxx/clj-slack "0.4.3"]
+                 [environ "1.0.0"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [bidi "1.20.3"]]
   :main ^:skip-aot slack-invite.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
